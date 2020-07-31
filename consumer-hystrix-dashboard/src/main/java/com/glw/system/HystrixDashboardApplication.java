35232f1cf2c4f46f6f42a4df4ae578dcf0321ea7
DragonV96 <510537547@qq.com>
2020-07-31 18:47:20 +0800

    [add]添加hystrix服务监控模块

---
+++ consumer-hystrix-dashboard/src/main/java/com/glw/system/HystrixDashboardApplication.java
@@ -0,0 +1,20 @@
+package com.glw.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author : glw
+ * @date : 2020/7/31
+ * @time : 16:45
+ * @Description : 启动类
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+}
