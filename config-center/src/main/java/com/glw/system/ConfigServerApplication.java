2b10dd69b80d345f958ec6c2c2e9c57d28ed934d
DragonV96 <510537547@qq.com>
2020-08-03 18:54:46 +0800

    [add]新增配置中心

---
+++ config-center/src/main/java/com/glw/system/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.glw.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author : glw
+ * @date : 2020/8/2
+ * @time : 20:56
+ * @Description : 启动类
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
